1d5c9bbc81ce96aac92491ceeb74221b4db080fd
dude-mansaa <39621383+dude-mansaa@users.noreply.github.com>
2018-07-04 19:09:09 +0530

    Update api_pwm.js

--- mjs_fs/api_pwm.js
+++ mjs_fs/api_pwm.js
@@ -1,22 +1,3 @@
-let PWM = {
-  // ## **`PWM.set(pin, freq, duty)`**
-  // Set and control the PWM. `pin` is a GPIO pin number, `freq` is
-  // frequency, in Hz. `freq` 0 disables PWM on the pin. `duty` specifies
-  // which fraction of the cycle is spent in "1" state: 0 is always off,
-  // 0.5 is a square wave, 1 is always on.
-  // Return: true - success, false - failure.
-  //
-  // Example:
-  // ```javascript
-  // PWM.set(pin, 50, 2.73);
-  // ```
-  // Note:
-  // on ESP32 we use 8 channels and 4 timers.
-  // Each `PWM.set()` call with new pin number assigns a new channel.
-  // If we already have a timer running at the specified frequency,
-  // we use it instead of assigning a new one.
-  set: ffi('bool mgos_pwm_set(int, int, float)'),
-};
 load('api_gpio.js');
 load('api_timer.js');
 load('api_file.js');
@@ -179,4 +160,25 @@ let GetCurrent = function(){
 			"b": b.led.b
 	};
 	return r;
+};
+
+
+let PWM = {
+  // ## **`PWM.set(pin, freq, duty)`**
+  // Set and control the PWM. `pin` is a GPIO pin number, `freq` is
+  // frequency, in Hz. `freq` 0 disables PWM on the pin. `duty` specifies
+  // which fraction of the cycle is spent in "1" state: 0 is always off,
+  // 0.5 is a square wave, 1 is always on.
+  // Return: true - success, false - failure.
+  //
+  // Example:
+  // ```javascript
+  // PWM.set(pin, 50, 2.73);
+  // ```
+  // Note:
+  // on ESP32 we use 8 channels and 4 timers.
+  // Each `PWM.set()` call with new pin number assigns a new channel.
+  // If we already have a timer running at the specified frequency,
+  // we use it instead of assigning a new one.
+  set: ffi('bool mgos_pwm_set(int, int, float)'),
 };
